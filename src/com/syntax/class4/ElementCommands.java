2aa772a91137f865adeff332e3c29e5f22ac0bc4
Zackny <zarkocubura@gmail.com>
2022-03-24 16:40:44 -0500

    class4

---
+++ src/com/syntax/class4/ElementCommands.java
@@ -0,0 +1,35 @@
+package com.syntax.class4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ElementCommands {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://www.uitestpractice.com/Students/Form");
+        WebElement merriedRadioButton=driver.findElement(By.xpath("(//input[@name = 'optradio'])[1]"));
+       boolean ismerriedRadioButtonEnabled= merriedRadioButton.isEnabled();
+        System.out.println(ismerriedRadioButtonEnabled);
+
+        if(!ismerriedRadioButtonEnabled){
+            System.out.println("Not enabled");
+        }else{
+            System.out.println("Enabled");
+        }
+      boolean ismerriedRadioButtonDisplayed=merriedRadioButton.isDisplayed();
+        System.out.println(ismerriedRadioButtonDisplayed);
+
+        boolean ismerriedRadioButtonSelected=merriedRadioButton.isSelected();
+        System.out.println("Before clicking "+ismerriedRadioButtonSelected);
+        merriedRadioButton.click();
+        Thread.sleep(3000);
+        boolean ismarriedRadioButtonSelected=merriedRadioButton.isSelected();
+        System.out.println("After clicking "+ismerriedRadioButtonSelected);
+
+        driver.quit();
+
+    }
+}
---
+++ src/com/syntax/class4/GetAllLinksFromEbuy.java
@@ -0,0 +1,30 @@
+package com.syntax.class4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class GetAllLinksFromEbuy {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","Driver/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://ebuy.com");
+        List<WebElement> alllinks = driver.findElements(By.tagName("a"));
+        System.out.println("Find elements "+alllinks.size());
+
+        for (WebElement link:alllinks
+             ) {
+            String linkText=link.getText();
+            String fullLink=link.getAttribute("href");
+            if(!linkText.isEmpty()){
+                System.out.println(linkText+" "+fullLink);
+            }
+
+        }
+
+
+    }
+}
